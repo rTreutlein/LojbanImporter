84c9c4b47b55a96a9068d74bee70ad6d6d7e330a
Roman Treutlein <roman@hansonrobotics.com>
2016-07-21 13:03:19 +0200

    Now using jbovlast xml export as data source

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,48 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds              #-}
+{-# LANGUAGE PartialTypeSignatures  #-}
 module Main where
 
 import OpenCog.Lojban.Lujvo
 import OpenCog.Lojban.Selmaho
+import OpenCog.Lojban.Gismu
 import OpenCog.AtomSpace
 
 import System.Environment
+
+import Text.XML.HXT.Core
+import qualified Data.Map as M
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
-        [t,filePath] -> do
-            file <- readFile filePath
-            case t of
-                "l" -> let parsed = mapM_ parseLujvoLine (lines file)
-                       in runOnNewAtomSpace (parsed >> debug)
-                "s" -> do
-                    map <- parseSelmao file
-                    print map
-        _ -> putStrLn "Please give to args (l (lujvo)| s (selmaho)) - file name"
+        (src:_) -> application src
+        _ -> putStrLn "Please provide source file"
+
+application :: String -> IO ()
+application src = do
+    gismu <- runX (readDocument [] src >>> getChildren >>> getValsi >>> getGismu)
+    cmavo <- runX (readDocument [] src >>> getChildren >>> getValsi >>> getCmavo)
+    print $ M.fromListWith (++) $ map f cmavo
+    where f (s,c) = (takeWhile p s,[c])
+          p e = e `notElem` "1234567890*"
+
+
+getGismu :: ArrowXml a => a XmlTree String
+getGismu = hasAttrValue "type" (== "gismu")
+           >>>
+           getAttrValue "word"
+
+getCmavo :: ArrowXml a => a XmlTree (String,String)
+getCmavo = hasAttrValue "type" (== "cmavo")
+           >>>
+           (getChildren >>> hasName "selmaho" /> getText)
+           &&&
+           getAttrValue "word"
+
+getValsi :: ArrowXml a => a XmlTree XmlTree
+getValsi = hasName "dictionary"
+           />
+           hasName "direction"
+           />
+           hasName "valsi"
